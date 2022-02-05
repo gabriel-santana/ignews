a11eb561f48930cd9afefea86d479e2324155fb1
Gabriel Santana <gabrielsantana77@hotmail.com>
2022-02-05 18:56:07 -0300

    webhook

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -4,8 +4,9 @@ import { fauna } from "../../../services/fauna";
 import { stripe } from '../../../services/stripe';
 
 export async function saveSubscription(
-  subscritionId: string,
+  subscriptionId: string,
   customerId: string,
+  createAction = false,
 ) {
   const userRef = await fauna.query(
     q.Select(
@@ -19,7 +20,7 @@ export async function saveSubscription(
     )
   )
 
-  const subscription = await stripe.subscriptions.retrieve(subscritionId)
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId)
 
   const subscriptionData = {
     id: subscription.id,
@@ -28,10 +29,27 @@ export async function saveSubscription(
     price_id: subscription.items.data[0].price.id,
   }
 
-  await fauna.query(
-    q.Create(
-      q.Collection('subscriptions'),
-      { data: subscriptionData }
+  if (createAction) {
+    await fauna.query(
+      q.Create(
+        q.Collection('subscriptions'),
+        { data: subscriptionData }
+      )
     )
-  )
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(
+            q.Match(
+              q.Index('subscription_by_id'),
+              subscriptionId
+            )
+          )
+        ),
+        { data: subscriptionData }
+      )
+    )
+  }
 }
