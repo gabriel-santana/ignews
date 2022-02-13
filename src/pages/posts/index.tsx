2900aeb4e32475fcb00b23e0391dc2fc633c152a
Gabriel Santana <gabrielsantana77@hotmail.com>
2022-02-13 10:30:23 -0300

    posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,25 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+import Link from 'next/link';
+
+import { getPrismicClient } from '../../services/prismic';
+
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }) {
   return (
     <>
       <Head>
@@ -10,23 +28,47 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div  className={styles.posts}>
-          <a href='#'>
-            <time>12 de março de 2021</time>
-            <strong>titulo do post</strong>
-            <p>um breve paragrafo do post</p>
-          </a>
-          <a href='#'>
-            <time>12 de março de 2021</time>
-            <strong>titulo do post</strong>
-            <p>um breve paragrafo do post</p>
-          </a>
-          <a href='#'>
-            <time>12 de março de 2021</time>
-            <strong>titulo do post</strong>
-            <p>um breve paragrafo do post</p>
-          </a>
+          { posts.map(post => (
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          )) }
+          
         </div>
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query<any>([
+    Prismic.predicates.at('document.type', 'publication')
+  ], {
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100,
+  })
+
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+
+  return {
+    props: { posts }
+  }
 }
